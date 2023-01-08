d4a84603b81561cf142caffa112c9c6ef6949da5
Harky29 <oluwaraphael16@gmail.com>
2023-01-08 22:02:24 +0000

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+	"""My addition function, To ppprint the sum of 1 and 2"""
+	from add_0 import add
+	a = 1
+	b = 2
+	print("{} + {} = {}".format(a,b, addd(a, b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,2 @@
+0x02-python-import_modules
+In this project, I am introduced to python modules.
